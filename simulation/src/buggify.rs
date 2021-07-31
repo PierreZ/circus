d29e5b0ea0bf9086364ae116702408472b31adea
Pierre Zemb <contact@pierrezemb.fr>
2021-07-31 12:14:44 +0200

    doc: add example for buggify

--- simulation/src/buggify.rs
+++ simulation/src/buggify.rs
@@ -74,6 +74,43 @@ pub fn disable_buggify() {
 /// 1. Enabled uses of `buggify!` have a 5% chance of evaluating to true
 ///
 /// A good blogpost about buggify can be found [here](https://transactional.blog/simulation/buggify.html).
+///
+/// ## Getting started with `buggify!`
+///
+/// ```rust
+/// extern crate circus_simulation;
+///
+/// use circus_simulation::buggify;
+/// use circus_simulation::buggify::*;
+/// use circus_simulation::deterministic::random::Random;
+/// use tracing::Level;
+///
+/// fn main() {
+///    // init random with a seed
+///    let random = Random::new_with_seed(42);
+///
+///    tracing_subscriber::fmt()
+///        .with_max_level(Level::DEBUG)
+///        .init();
+///
+///    // enables buggify
+///    enable_buggify(random);
+///
+///    for i in 0..10 {
+///        // this block has a 0.05% chance to be run
+///        // which is iteration 8 for seed 42
+///        if buggify!() {
+///            tracing::info!("buggified at iteration {}", i);
+///        }
+///    }
+///
+///    // buggify can also accept a probability
+///    if buggify!(1.0) {
+///        tracing::info!("buggified with a 100% probability!");
+///    }
+/// }
+/// ```
+///
 macro_rules! buggify {
     ($probability:expr) => {{
         let line = format!("{}:{}", file!(), line!());
@@ -120,7 +157,10 @@ mod tests {
             assert!((*data).is_some());
 
             let map = BUGGIFIER_INSTANCE.buggified_lines.lock();
-            assert!((*map).contains_key("simulation/src/buggify.rs:114"));
+            assert_eq!((*map).len(), 1);
+            for key in (*map).keys() {
+                assert!(key.starts_with("simulation/src/buggify.rs:"));
+            }
         }
 
         disable_buggify();
